5778b38c2144c8496b7bb4f934c513e063e0ba41
SeungYong Oh <osy090712@gmail.com>
2021-07-11 01:28:32 +0900

    feat - add hello react

---
+++ react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello, react!
+      </div>
+    )
+  }
+}
+
+export default App;
