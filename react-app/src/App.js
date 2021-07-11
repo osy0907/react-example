a3bd7269339cc36db6264daff0a689a82408b531
SeungYong Oh <osy090712@gmail.com>
2021-07-12 02:05:16 +0900

    feat - event state props, render function

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {  // App이 내부적으로 사용할 상태는 state를 사용한다.
+      mode:'welcome',
       subject:{title:"WEB", sub:"World wide web!"},
+      welcome:{title:'Welcome', desc:'Hello, React!'},
       contents: [
         {id:1, title:"HTML", desc:"HTML is for information"},
         {id:2, title:'CSS', desc:'CSS is for design'},
@@ -17,16 +19,32 @@ class App extends Component {
     }
   }
   render() {
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title={this.state.subject.title}
           sub={this.state.subject.sub}>
-        </Subject>
+        </Subject> */}
+        <header>
+            <h1><a href="/" onClick={function(e){ /* react에서는 이벤트 함수가 호출될 때 첫 번째 매개변수의 값으로 event라는 객체를 주입해 주기로 약속돼 있다. */
+            console.log(e.metaKey);
+            e.preventDefault();
+            alert("osy");
+            }}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+        </header>  
         <Subject title="React" sub="so fun"></Subject>
         <TOC
         data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     )
   }
