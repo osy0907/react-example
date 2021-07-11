1f0a9c5d10a0a329dc4a229db1394ce61c1b983e
SeungYong Oh <osy090712@gmail.com>
2021-07-11 21:43:19 +0900

    feat - props

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import TOC from './components/TOC';
+import Subject from './components/Subject';
+import Content from './components/Content';
 import { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, react!
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject title="React" sub="so fun"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     )
   }
