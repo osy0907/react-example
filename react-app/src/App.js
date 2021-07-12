0e953f8e5d44e8ed9fde1c43f45f057b278c2796
SeungYong Oh <osy090712@gmail.com>
2021-07-13 00:07:54 +0900

    feat - component event

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {  // App이 내부적으로 사용할 상태는 state를 사용한다.
-      mode:'welcome',
+      mode:'read',
+      selected_content_id:2,
       subject:{title:"WEB", sub:"World wide web!"},
       welcome:{title:'Welcome', desc:'Hello, React!'},
       contents: [
@@ -24,25 +25,46 @@ class App extends Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
     } else if (this.state.mode === 'read') {
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+      var i = 0;
+      while (i < this.state.contents.length) {
+        var data = this.state.contents[i];
+        if (data.id === this.state.selected_content_id) {
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
-        </Subject> */}
-        <header>
-            <h1><a href="/" onClick={function(e){ /* react에서는 이벤트 함수가 호출될 때 첫 번째 매개변수의 값으로 event라는 객체를 주입해 주기로 약속돼 있다. */
-            console.log(e.metaKey);
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'})
+          }.bind(this)}
+          >
+        </Subject>
+        {/* <header>
+            <h1><a href="/" onClick={function(e){ // react에서는 이벤트 함수가 호출될 때 첫 번째 매개변수의 값으로 event라는 객체를 주입해 주기로 약속돼 있다.
+            console.log(e.metaKey);               // onClick func 안에서는 this가 컴포넌트 자기자신을 가르키지 않고 아무값도 셋팅되어 있지 않음.
             e.preventDefault();
-            alert("osy");
-            }}>{this.state.subject.title}</a></h1>
+            // this.state.mode = 'welcome';
+            this.setState({
+              mode:'welcome'
+            });
+            }.bind(this)}>{this.state.subject.title}</a></h1> // func 내부 this는 아무것도 가르키지 않는데 bind함수에 객체를 인자로 주면 객체가 주입 된 해당 함수를 리턴
             {this.state.subject.sub}
-        </header>  
+        </header>  */}
         <Subject title="React" sub="so fun"></Subject>
         <TOC
+        onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}
         data={this.state.contents}></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
