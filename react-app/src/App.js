3158521c045dcb25337d2472c6475c9cc20ed99a
SeungYong Oh <osy090712@gmail.com>
2021-07-17 23:52:42 +0900

    feat - add onSubmit event

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -40,7 +40,10 @@ class App extends Component {
       }
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'create') {
-      _article = <CreateContent></CreateContent>
+      _article = <CreateContent onSubmit={function(_title, _desc) {
+        // add content to this.state.contents
+        console.log(_title, _desc);
+      }}></CreateContent>
     }
 
     return (
