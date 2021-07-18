0e62fe1e1d1ce475661489cc06bc30ad66f994c1
SeungYong Oh <osy090712@gmail.com>
2021-07-18 23:58:49 +0900

    feat - update

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -42,8 +42,8 @@ class App extends Component {
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
-      var _content = this.getContent();
-      _article = <ReadContent title={_content._title} desc={_content._desc}></ReadContent>
+      var _content = this.getReadContent()
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     } else if (this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         // add content to this.state.contents
@@ -64,11 +64,17 @@ class App extends Component {
       }.bind(this)}></CreateContent>
     } else if (this.state.mode === 'update') {
       _content = this.getReadContent();
-      _article = <UpdateContent data={_content} onSubmit={function(_title, _desc) {
-        this.max_content_id = this.max_content_id+1;
-        var _contents = this.state.contents.concat(
-          {id:this.max_content_id, title:_title, desc:_desc}
-        );
+      _article = <UpdateContent data={_content} onSubmit={
+        function(_id, _title, _desc) {
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while (i < _contents.length) {
+          if (_contents[i].id === _id) {
+            _contents[i] = {id:_id, title:_title, desc:_desc};
+            break;
+          }
+          i = i + 1;
+        }
         this.setState({
           contents:_contents
         });
